069ebc67ccd7c67b954cba791a2609bb5f03fe56
YeetMeLol101 <57913656+YeetMeLol101@users.noreply.github.com>
2024-02-06 10:10:52 +0000

    Update Program.cs

--- Stacks/Program.cs
+++ Stacks/Program.cs
@@ -1,96 +1,14 @@
-﻿namespace Stacks
+namespace Stacks
 {
     internal class Program
     {
-        const int MaxSize = 4;
-        
-        static bool IsFull(int TOP)
-        {
-            return (TOP == MaxSize - 1);
-
-            /*Does the same as:
-            if (TOP == MaxSize - 1)
-                return true;
-            else
-                return false;
-            */
-            
-        }
-
-        static bool IsEmpty(int TOP)
-        {
-            return (TOP == -1);
-
-            /*Does the same as:
-            if (TOP == -1)
-                return true;
-            else
-                return false;*/
-
-        }
-
-        static void Push(string[] STACK, ref int TOP, string DATA)
-        {
-            if (IsFull(TOP) /*== true*/)
-            {
-                Console.WriteLine($"Stack is full -- {DATA} could not be loaded");
-            }
-            else
-            {
-                TOP = TOP + 1;
-                STACK[TOP] = DATA;
-            }
-        }
-
-        static string Pop(string[] STACK, ref int TOP)
-        {
-            string PoppedItem;
-
-            if (IsEmpty(TOP) /*== true*/)
-            {
-                Console.WriteLine($"Stack is empty -- nothing to pop");
-                PoppedItem = "";
-            }
-            else
-            {
-                PoppedItem = STACK[TOP];
-                TOP = TOP - 1;
-            }
-            return PoppedItem;
-        }
-
-        static string Peek(string[] STACK, ref int TOP)
-        {
-            string PeekedItem;
-
-            if (IsEmpty(TOP) )
-            {
-                Console.WriteLine($"Stack is empty -- nothing to peek");
-                PeekedItem = "";
-            }
-            else
-            {
-                PeekedItem = STACK[TOP];
-                TOP = TOP - 1;
-            }
-            return PeekedItem;
-        }
-
-        static void PrintStack(ref string[] STACK)
-        {
-            foreach (string value in STACK) //Does the same as: [for (int i; i < TOP; i++)] with PrintStack referencing TOP
-            {
-                Console.WriteLine(value);
-            }
-        }
         static void Main(string[] args)
         {
-            string[] stack = new string[MaxSize];
-            int top = -1;
+            Stack stack = new Stack();
 
-            Push(stack, ref top, "One");
-            Push(stack, ref top, "Two");
-            Push(stack, ref top, "Three");
+            stack.Push("One");//Fix these later
+            stack.Push("Two");
+            stack.Push("Three");
             Push(stack, ref top, "Four");
 
             Push(stack, ref top, "Five"); //Stack Overflow at this point
@@ -99,6 +17,8 @@
             Push(stack, ref top, "Eight");
             Push(stack, ref top, "Nine");
             Push(stack, ref top, "Ten");
+
+            PrintStack(ref stack);
 
             Console.WriteLine(Pop(stack, ref top));
             Console.WriteLine(Pop(stack, ref top));
@@ -112,8 +32,93 @@
             Console.WriteLine(Peek(stack, ref top));
             Console.WriteLine(Peek(stack, ref top));
 
-            PrintStack(ref stack);
+            
 
+        }
+    }
+
+    class Stack
+    {
+        private const int MaxSize = 4;
+        private string[] STACK = new string[MaxSize];
+        private int TOP = -1;
+        public bool IsFull()
+        {
+            return (TOP == MaxSize - 1);
+
+            /*Does the same as:
+            if (TOP == MaxSize - 1)
+                return true;
+            else
+                return false;
+            */
+
+        }
+
+        public bool IsEmpty()
+        {
+            return (TOP == -1);
+
+            /*Does the same as:
+            if (TOP == -1)
+                return true;
+            else
+                return false;*/
+
+        }
+
+        public void Push(string DATA)
+        {
+            if (IsFull() /*== true*/)
+            {
+                Console.WriteLine($"Stack is full -- {DATA} could not be loaded");
+            }
+            else
+            {
+                TOP = TOP + 1;
+                STACK[TOP] = DATA;
+            }
+        }
+
+        public string Pop()
+        {
+            string PoppedItem;
+
+            if (IsEmpty() /*== true*/)
+            {
+                Console.WriteLine($"Stack is empty -- nothing to pop");
+                PoppedItem = "";
+            }
+            else
+            {
+                PoppedItem = STACK[TOP];
+                TOP = TOP - 1;
+            }
+            return PoppedItem;
+        }
+
+        public string Peek()
+        {
+            string PeekedItem;
+
+            if (IsEmpty())
+            {
+                Console.WriteLine($"Stack is empty -- nothing to peek");
+                PeekedItem = "";
+            }
+            else
+            {
+                PeekedItem = STACK[TOP];
+            }
+            return PeekedItem;
+        }
+
+        public void PrintStack()
+        {
+            foreach (string value in STACK) //Does the same as: [for (int i; i < TOP; i++)] with PrintStack referencing TOP
+            {
+                Console.WriteLine(value);
+            }
         }
     }
 }
